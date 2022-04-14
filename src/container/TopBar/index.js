5d4a12a9f5e1d973ec088f1e055a031c42eebae9
anand-vyas-gg <anand.vyas@globalgarner.com>
2022-04-14 15:50:42 +0530

    some minor changes

--- src/container/TopBar/index.js
+++ src/container/TopBar/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./index.scss";
 import { UpperHeader } from "./UpperHeader";
 import { LowerHeader } from "./LowerHeader";
 
@@ -6,7 +7,7 @@ const TopBar = () => {
   return (
     <header className="header">
       <UpperHeader />
-      <LowerHeader />
+      {/* <LowerHeader /> */}
     </header>
   );
 };
